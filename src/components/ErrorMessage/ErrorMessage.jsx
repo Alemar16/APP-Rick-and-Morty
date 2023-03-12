68868441b5734ded93d81c83fa08f21de952df80
Armando Martinez <alemar.martinez16@gmail.com>
2023-03-12 03:08:28 -0300

    Estilo del Componente ErrorMessage

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import styles from "./ErrorMessage.module.css";
 
 export default function ErrorMessage(props) {
+  const [show, setShow] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShow(false);
+      props.onClose();
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [props]);
+
+  const handleClick = () => {
+    setShow(false);
+    props.onClose();
+  };
   return (
-    <div className={styles.container}>
-      <p className={styles.text}>{props.message}</p>
-    </div>
+    <>
+      {show && (
+        <div className={styles.container}>
+          <p>{props.message}</p>
+          <div className={styles.close} onClick={handleClick}>
+            
+          </div>
+        </div>
+      )}
+    </>
   );
 }
