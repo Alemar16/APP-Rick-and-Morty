56d24ad04765d4bfe179d5791cbda513a0b3f1b1
Armando Martinez <alemar.martinez16@gmail.com>
2023-03-13 14:18:20 -0300

    Logo estilos establecido

--- src/components/Nav/Logo/Logo.jsx
+++ src/components/Nav/Logo/Logo.jsx
@@ -4,7 +4,11 @@ import styles from "./Logo.module.css";
 export default function Logo() {
   return (
     <div className={styles.logo}>
-      <img src="/Rick_and_Morty_Logo-transformed.png" alt="Logo" />
+      <img
+        src="/Rick_and_Morty_Logo-transformed.png"
+        className={styles.logoImg}
+        alt="Logo"
+      />
     </div>
   );
 }
