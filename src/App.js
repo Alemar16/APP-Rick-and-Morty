ac59f262d81d4ab3b6872ecc96a63b1a621763b9
Armando Martinez <alemar.martinez16@gmail.com>
2023-03-12 22:39:56 -0300

    instalacion dependencias React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import "./App.css";
-import Cards from "./components/Targetas/Cards/Cards";
-import { useState } from "react"; //destructuring
+import { useState } from "react";
 import Nav from "./components/Nav/Nav";
 import Titulo from "./components/Titulo/Titulo";
 import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-
+import Cards from "./components/Targetas/Cards/Cards";
 
 function App() {
-/*     const example = {
-      name: "Rick Sanchez",
-      species: "Human",
-      gender: "Male",
-      image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-  }; */ 
-
   const [characters, setCharacters] = useState([]);
   const [error, setError] = useState(null);
 
   function onSearch(character) {
     const characterId = parseInt(character);
     if (isNaN(characterId)) {
-    setError("Por favor ingresa un valor numérico.");
-    return;
-  }
+      setError("Por favor ingresa un valor numérico.");
+      return;
+    }
     if (characters.some((char) => char.id === characterId)) {
       setError(`El personaje con ID ${characterId} ya está mostrado.`);
       return;
@@ -33,24 +25,22 @@ function App() {
       .then((data) => {
         if (data.name) {
           setCharacters((oldChars) => [...oldChars, data]);
-          setError(null)
-        }else {
+          setError(null);
+        } else {
           setError(`No existe un personaje con el ID ${characterId}`);
         }
       });
-      }
-    function handleCloseCard(id) {
-      setCharacters((oldChars) => oldChars.filter((char) => char.id !== id));
-    }
+  }
 
+  function handleCloseCard(id) {
+    setCharacters((oldChars) => oldChars.filter((char) => char.id !== id));
+  }
 
-  
   return (
-    <div className="App" style={{ padding: "10px" }}>
-      
+    <div className="App">
       <Nav onSearch={onSearch} />
       <Titulo />
-      {error && <ErrorMessage message={error}/>}
+      {error && <ErrorMessage message={error} />}
       <Cards characters={characters} onCloseCard={handleCloseCard} />
     </div>
   );
