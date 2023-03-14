b3321fa45c0b6aa975de7ac5ddc9c882b20b75f1
Armando Martinez <alemar.martinez16@gmail.com>
2023-03-14 17:22:16 -0300

    Cambios de estilos en Detail

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -26,12 +26,24 @@ export default function Detail(props) {
     <div className={styles.container}>
       <div className={styles.containerInfo}>
         <div>
-          <h1>Name: {character.name}</h1>
-          <h1>Gender: {character.gender}</h1>
-          <h1>Specie: {character.species}</h1>
-          <h1>Status: {character.status}</h1>
-          <h1>Origin: {character.origin?.name}</h1>
-          <h1>Location: {character.location?.name}</h1>
+          <h1>
+            <span className={styles.texto}>Name:</span> <span className={styles.text2}>{character.name}</span> 
+          </h1>
+          <h1>
+            <span className={styles.texto}>Gender:</span> {character.gender}
+          </h1>
+          <h1>
+            <span className={styles.texto}>Specie:</span> {character.species}
+          </h1>
+          <h1>
+            <span className={styles.texto}>Status:</span> {character.status}
+          </h1>
+          <h1>
+            <span className={styles.texto}>Origin:</span> {character.origin?.name}
+          </h1>
+          <h1>
+            <span className={styles.texto}>Location:</span> {character.location?.name}
+          </h1>
         </div>
         <img src={character.image} alt="" />
       </div>
