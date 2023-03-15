337018edbf4da876cba4745f246a796b2018c5c2
Armando Martinez <alemar.martinez16@gmail.com>
2023-03-15 11:20:46 -0300

    Funcionalidad establecida para Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -6,20 +6,28 @@ export default function Form(props) {
   
   //eslint-disable-next-line
   const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  const validLettersAndNumbers = new RegExp(/^(?=.*[a-z])(?=.*[0-9])/)
 
   const validate = (inputs) => {
     //inputs --. {userName: "Feli", password: ""}
-    const errors = {}
-    if (regexEmail.test(inputs.userName)) {
-      errors.userName = "debe ser un email"
-    }
-    if(!inputs.userName)
-    errors.userName = "No puede ser vacio" 
+  const errors = {}
+  if (regexEmail.test(inputs.userName)) {
+    errors.userName = "debe ser un email"
+  }
+  if (!inputs.userName){
+    errors.userName = "No puede ser vacio"
   }
   if (inputs.userName.length > 35) {
-      errors.userName = "No puede tener mas de 35 caracteres"
+    errors.userName = "No puede tener mas de 35 caracteres"
+  }
+  if (!validLettersAndNumbers.test(inputs.password)) {
+    errors.password = "Debe contener mínimo un número"
+  }
+  if (inputs.password.length < 6 || inputs.password.length > 10) {
+    errors.password = "Debe tener entre 6 y 10 caracteres"
     }
-
+    return errors
+}
   const [userData, setUserData] = useState({
     userName: "",
     password: "",
@@ -30,10 +38,15 @@ export default function Form(props) {
     });
   const handleChange = (event) => {
     const { name, value } = event.target
+    //username: abc123@gmail.com / password: ******
     setUserData({
-      ...userData,
+      ...userData,//compara una copia con el DOM virtual
       [name]: value,
     })
+    setErrors(validate({
+      ...userData,
+      [name]:value
+    }))
   }
   useEffect(() => {
     console.log(userData)
@@ -53,14 +66,22 @@ export default function Form(props) {
         value={userData.userName}
         name="userName"
         onChange={handleChange}
+        className={errors.userName && styles.warning}
       />
+      {errors.userName ? (
+        <p style={{ color: "red" }}>{errors.userName}</p>
+      ) : null}
       <label htmlFor="">Contraseña:</label>
       <input
         type="password"
         value={userData.password}
         name="password"
         onChange={handleChange}
+        className={errors.password && styles.warning}
       />
+      {errors.password ? (
+        <p style={{ color: "red" }}>{errors.password}</p>
+      ) : null}
       <br />
       <button>Login</button>
     </form>
